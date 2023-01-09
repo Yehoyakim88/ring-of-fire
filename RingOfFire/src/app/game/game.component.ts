7e28e36278544d4d31293c5c74c73a06dc17044e
Yehoyakim88 <yehoyakim@mail.de>
2023-01-09 08:07:30 +0100

    addPlayer Button with Dialog implemented

--- RingOfFire/src/app/game/game.component.ts
+++ RingOfFire/src/app/game/game.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
+
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
   game : Game;
   currentCard : string = '';
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
 
   }
 
@@ -43,5 +46,14 @@ export class GameComponent implements OnInit {
         this.pickCardAnimation = false;
       }, 1000);
     }
+  }
+
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 }
